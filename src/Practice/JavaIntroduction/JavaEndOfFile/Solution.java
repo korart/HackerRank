f5585f0f4477fbd1d0bd284a512f1dbc17962f88
Artem Korolev <korart@gmail.com>
2020-11-24 10:26:18 +0300

    Java EndOfFile Successfully

---
+++ src/Practice/JavaIntroduction/JavaEndOfFile/Solution.java
@@ -0,0 +1,18 @@
+package Practice.JavaIntroduction.JavaEndOfFile;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int counter = 1;
+        while (in.hasNext()) {
+            String a = in.nextLine();
+            System.out.println(counter + " " + a);
+            counter++;
+        }
+    }
+}
