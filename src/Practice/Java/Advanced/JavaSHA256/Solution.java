5ef0a028ceae95f63bf8bb80c61d79dc40a6fcbf
Artem Korolev <korart@gmail.com>
2020-11-24 15:44:25 +0300

    Java SHA256 Successfully

--- src/Practice/Java/Advanced/JavaSHA256/Solution.java
+++ src/Practice/Java/Advanced/JavaSHA256/Solution.java
@@ -1,6 +1,9 @@
 package Practice.Java.Advanced.JavaSHA256;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class Solution {
@@ -11,13 +14,23 @@ public class Solution {
         try {
             String input = reader.readLine();
             reader.close();
-            System.out.println();
-        } catch (IOException e) {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            System.out.println(bytesToHex(encodedhash));
+        } catch (IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
 
-    public static class SHA256 {
-
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
