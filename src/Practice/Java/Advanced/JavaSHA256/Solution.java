4a0b8e6f26e06b5686bb8d41e4a4a6f4c2b73f4a
Artem Korolev <korart@gmail.com>
2020-11-24 15:14:31 +0300

    Java SHA256 Start

---
+++ src/Practice/Java/Advanced/JavaSHA256/Solution.java
@@ -0,0 +1,23 @@
+package Practice.Java.Advanced.JavaSHA256;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String input = reader.readLine();
+            reader.close();
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class SHA256 {
+
+    }
+}
